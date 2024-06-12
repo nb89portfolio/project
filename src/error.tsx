69ba194161ece72673132fd8455eee27702c4473
Navraj <navraj.bains.89@outlook.com>
2024-06-11 23:15:03 -0400

    Initial fallback component completed.

--- src/error.tsx
+++ src/error.tsx
@@ -3,13 +3,57 @@
 import { ErrorInfo, ReactNode } from "react";
 import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 
+type DefinedError = {
+  name: string;
+  message: string;
+  stack: string;
+};
+
+function assignErrorObjectProperty(
+  key: keyof Error,
+  state: "unknown" | "undefined"
+) {
+  return `Error ${key} is ${state}.` as string;
+}
+
+function defineErrorObject(error: any) {
+  const isError =
+    error instanceof Error ||
+    error instanceof SyntaxError ||
+    error instanceof TypeError;
+
+  const name = isError
+    ? error.name
+    : assignErrorObjectProperty("name", "unknown");
+  const message = isError
+    ? error.message
+    : assignErrorObjectProperty("message", "unknown");
+
+  const stack = isError
+    ? error.stack
+    : assignErrorObjectProperty("stack", "unknown");
+
+  const definedStack =
+    stack !== undefined
+      ? stack
+      : assignErrorObjectProperty("stack", "undefined");
+
+  return {
+    name,
+    message,
+    stack: definedStack,
+  } as DefinedError;
+}
+
 function Fallback({ error, resetErrorBoundary }: FallbackProps) {
+  const { name, message, stack } = defineErrorObject(error);
+
   return (
     <main>
-      <h2></h2>
-      <p></p>
-      <details></details>
-      <button>Reset Error</button>
+      <h2>{name}</h2>
+      <p>{message}</p>
+      <details>{stack}</details>
+      <button onClick={() => resetErrorBoundary()}>Reset Error</button>
     </main>
   );
 }
@@ -29,7 +73,7 @@ function onReset(
       }
 ) {}
 
-export default function Error({ children }: { children: ReactNode }) {
+export default function ErrorWrapper({ children }: { children: ReactNode }) {
   return (
     <ErrorBoundary
       FallbackComponent={Fallback}
