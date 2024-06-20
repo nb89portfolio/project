d5cc2d933c3980d0d10c53359d244b1588704930
Navraj <navraj.bains.89@outlook.com>
2024-06-20 13:25:30 -0400

    fixed input output component functionality.

--- src/input/output/component.tsx
+++ src/input/output/component.tsx
@@ -1,20 +1,44 @@
+import { Dispatch, SetStateAction } from "react";
+
 type ValidationInputOutputValidator = RegExp | string;
+
+type ValidationInputOutputSetState = Dispatch<
+  SetStateAction<{
+    [key: string]: {
+      isValid: boolean;
+      [key: string]: any;
+    };
+  }>
+>;
 
 type ValidationInputOutputProps = {
   name: string;
   value: string;
   validator: ValidationInputOutputValidator;
   prompt: string;
+  setState: ValidationInputOutputSetState;
 };
 
 function determinePrompt(
+  name: string,
   value: string,
   validator: ValidationInputOutputValidator,
-  prompt: string
+  prompt: string,
+  setState: ValidationInputOutputSetState
 ) {
   const isString = typeof validator === "string";
 
   const isValid = isString ? value === validator : validator.test(value);
+
+  setState((previousState) => {
+    return {
+      ...previousState,
+      [name]: {
+        ...previousState[name],
+        isValid,
+      },
+    };
+  });
 
   return isValid ? "" : prompt;
 }
@@ -24,8 +48,9 @@ export default function ValidationInputOutput({
   value,
   validator,
   prompt,
+  setState,
 }: ValidationInputOutputProps) {
-  const output = determinePrompt(value, validator, prompt);
+  const output = determinePrompt(name, value, validator, prompt, setState);
 
   return <output htmlFor={name}>{output}</output>;
 }
