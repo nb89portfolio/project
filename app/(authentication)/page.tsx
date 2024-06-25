658e0e57b27d0f36c97d7244511d90252db96202
Navraj <navraj.bains.89@outlook.com>
2024-06-25 15:27:33 -0400

    Templating signup.

---
+++ app/(authentication)/page.tsx
@@ -0,0 +1,33 @@
+export default function SignUp() {
+  return (
+    <main>
+      <h2>Sign Up</h2>
+      <form>
+        <fieldset>
+          <div>
+            <span>
+              <label></label>
+              <input></input>
+            </span>
+            <output></output>
+          </div>
+          <div>
+            <span>
+              <label></label>
+              <input></input>
+            </span>
+            <output></output>
+          </div>
+          <div>
+            <span>
+              <label></label>
+              <input></input>
+            </span>
+            <output></output>
+          </div>
+        </fieldset>
+        <button></button>
+      </form>
+    </main>
+  );
+}
--- app/(authentication)/types.tsx
+++
@@ -1,8 +0,0 @@
-export type AuthenticationField = {
-  label: string;
-  name: string;
-  type: "email" | "string" | "password";
-  placeholder: string;
-  validation: RegExp;
-  prompts: string;
-};
