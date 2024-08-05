59de04c4c6e197adf9f185bf1b434c332327ac05
Navraj <navraj.bains.89@outlook.com>
2024-08-05 19:32:31 -0400

    Main dummy component created.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Main from "@/src/components/main/component";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h2>home</h2>
-    </main>
-  );
+  const title = "home page";
+  const content = <></>;
+
+  return <Main title={title} content={content}></Main>;
 }
---
+++ src/components/main/component.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import styles from "./styles.module.css";
+
+type Props = {
+  title: string;
+  content: ReactNode;
+};
+
+export default function Main({ title, content }: Props) {
+  return (
+    <main className={styles.main}>
+      <h2>{title}</h2>
+      {content}
+    </main>
+  );
+}
