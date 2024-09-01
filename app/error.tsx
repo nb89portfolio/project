15603c1839d039b1b2dd43ad2825b1aca8472ff0
Navraj <navraj.bains.89@outlook.com>
2024-09-01 12:52:45 -0400

    Completed client side error reporting.

--- app/error.tsx
+++ app/error.tsx
@@ -1,76 +1,9 @@
 "use client";
 
-import { ErrorReport, ErrorRecords, NextJSError } from "@/src/error/types";
+import { NextJSError } from "@/src/error/types";
+import UseErrorRecord from "@/src/error/use";
 import Main from "@/src/main/component";
-import { useContext, useEffect } from "react";
-import reportError from "@/src/error/action";
-import { ErrorRecordContext } from "@/src/error/context";
-
-function defineError(error: NextJSError) {
-  const { name, message } = error;
-
-  const isStackDefined = error.stack !== undefined;
-
-  const stack = isStackDefined
-    ? (error.stack as string)
-    : "Stack is undefined.";
-
-  const isDigestDefined = error.digest !== undefined;
-
-  const digest = isDigestDefined
-    ? (error.digest as string)
-    : "Digest undefined.";
-
-  return { name, message, stack, digest } as ErrorReport;
-}
-
-function isDuplicate(report: ErrorReport, records: ErrorReport[]) {
-  const foundDuplicate = records.find((record) => {
-    return (
-      report.name === record.name &&
-      report.message === record.message &&
-      report.stack === record.stack &&
-      report.digest === record.digest
-    );
-  });
-
-  const hasDuplicate = foundDuplicate !== undefined;
-
-  return hasDuplicate as boolean;
-}
-
-function handleError(report: ErrorReport, errorState: ErrorRecords) {
-  const { records, setRecords, setStatus } = errorState;
-
-  const hasDuplciate = isDuplicate(report, records);
-
-  if (hasDuplciate) {
-    setStatus("Error has already been reported.");
-  } else {
-    setRecords([...records, report]);
-
-    reportError(report)
-      .then((response) => {
-        setStatus(response);
-      })
-      .catch((error) => {
-        const hasError = (error as Error).name !== undefined;
-
-        if (hasError) {
-          const { name, message, stack, digest } = defineError(error as Error);
-
-          const newMessage = `${name}, ${message}, ${stack}, ${digest}`;
-
-          setStatus(newMessage);
-        } else {
-          const message =
-            "Unknown error occured when client tries to report error to server.";
-
-          setStatus(message);
-        }
-      });
-  }
-}
+import { useState } from "react";
 
 export default function Error({
   error,
@@ -81,23 +14,10 @@ export default function Error({
 }) {
   const title = "Fatal Error";
 
-  const definedError = defineError(error);
-
-  const { name, message, stack } = definedError;
-
-  const errorState = useContext(ErrorRecordContext);
-
-  useEffect(() => {
-    handleError(definedError, errorState);
-  }, []);
-
   return (
     <Main title={title}>
-      <h3>{name}</h3>
-      <p>{message}</p>
-      <button onClick={() => reset()}>Reset Error</button>
-      <output>{errorState.status}</output>
-      <details>{stack}</details>
+      <UseErrorRecord error={error}></UseErrorRecord>
+      <button onClick={reset}>Reset Error</button>
     </Main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import LoadingWrapper from "@/src/loading/component";
-import ErrorBoundary from "@/src/error/context";
+import ErrorRecordProvider from "@/src/error/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <header>Test header</header>
-        <ErrorBoundary>
+        <ErrorRecordProvider>
           <LoadingWrapper>{children}</LoadingWrapper>
-        </ErrorBoundary>
+        </ErrorRecordProvider>
         <header>Test footer</header>
       </body>
     </html>
