96f083918d4e38fafdcfb783559373f4a18a3fa3
Navraj <navraj.bains.89@outlook.com>
2024-09-08 00:36:34 -0400

    Cleaned up url component.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,20 @@
-import Main from "@/src/main/component";
-import NavigationButton from "@/src/navigate/component";
-import UrlText from "@/src/url/component";
+import Main from '@/src/main/component';
+import NavigationButton from '@/src/navigate/component';
+import UseUrl from '@/src/url/component';
 
 export default function NotFound() {
-  const title = "Page Not Found";
+  const title = 'Page Not Found';
 
-  const preText = "The page that you are looking for,";
-  const postText = ", cannot be found.";
+  const preText = 'The page that you are looking for,';
+  const postText = ', cannot be found.';
 
   return (
     <Main title={title}>
       <p>
-        <UrlText preText={preText} postText={postText}></UrlText>
+        <UseUrl preText={preText} postText={postText}></UseUrl>
       </p>
-      <NavigationButton route={"back"}></NavigationButton>
-      <NavigationButton route={"home"}></NavigationButton>
+      <NavigationButton route={'back'}></NavigationButton>
+      <NavigationButton route={'home'}></NavigationButton>
     </Main>
   );
 }
