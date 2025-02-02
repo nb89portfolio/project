95372735e9c64d347aa2905592aac402f871b0c8
nb89portfolio <navraj.bains.89@outlook.com>
2025-02-01 19:58:11 -0500

    Server serves html, javascript, and css correctly.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,27 @@
 import { createServer } from 'node:http';
+import fs from 'fs';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  if (req.url == '/') {
+    const readStream = fs.createReadStream('./src/client/index.html');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    readStream.pipe(res);
+  }
+
+  if (req.url == '/styles.css') {
+    const readStream = fs.createReadStream('./src/client/styles.css');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    readStream.pipe(res);
+  }
+
+  if (req.url == '/script.js') {
+    const readStream = fs.createReadStream('./src/client/script.js');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    readStream.pipe(res);
+  }
 });
 
 server.listen(port, hostname, () => {
