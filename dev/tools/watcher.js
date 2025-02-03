ed7eaba8f6a880ae45f771c3d1fd6aead7b08cab
nb89portfolio <navraj.bains.89@outlook.com>
2025-02-02 20:18:45 -0500

    Watcher semi complete.

--- dev/tools/watcher.js
+++ dev/tools/watcher.js
@@ -3,21 +3,42 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const fs_1 = require("fs");
 const path_1 = require("path");
-const script = 'npm run dev';
-const runScript = () => {
-    (0, child_process_1.exec)(script, (err, stdout, stderr) => {
+let server = null;
+const runFunctions = () => {
+    (0, child_process_1.exec)('node ./dev/tools/copyFiles.js');
+    (0, child_process_1.exec)('node ./dev/server/server.js');
+};
+const startServer = () => {
+    if (server) {
+        server.kill();
+    }
+    server = (0, child_process_1.spawn)('node', [(0, path_1.join)(__dirname, '../../dist/server/server.js')], {
+        stdio: 'inherit',
+    });
+    server.on('close', (code) => {
+        if (code !== null) {
+            console.log(`Server stopped with exit code ${code}`);
+        }
+    });
+    console.log('Server started.');
+};
+const compileAndRun = () => {
+    (0, child_process_1.exec)('tsc', { cwd: (0, path_1.join)(__dirname, '../../') }, (err, stdout, stderr) => {
         if (err) {
-            console.error(`Error: ${stderr}`);
+            console.error(`Error @ compile: ${stderr}`);
         }
         else {
-            console.log(`Output: ${stdout}`);
+            console.log(stdout);
+            runFunctions();
+            startServer();
         }
     });
 };
-(0, fs_1.watch)((0, path_1.join)('./src'), { recursive: true }, (eventType, filename) => {
+(0, fs_1.watch)((0, path_1.join)(__dirname, '../../src'), { recursive: true }, (eventType, filename) => {
     if (filename && ['.ts', '.html', '.css'].includes((0, path_1.extname)(filename))) {
-        console.log(`${filename} file changed. Running script...`);
-        runScript();
+        console.log(`${filename} file changed. Recompiling and restarting server...`);
+        compileAndRun();
     }
 });
-runScript();
+// Initial compile and run
+compileAndRun();
--- src/tools/watcher.ts
+++ src/tools/watcher.ts
@@ -1,24 +1,52 @@
-import { exec } from 'child_process';
+import { spawn, exec, ChildProcess } from 'child_process';
 import { watch } from 'fs';
 import { join, extname } from 'path';
 
-const script = 'npm run dev';
+let server: ChildProcess | null = null;
 
-const runScript = () => {
-  exec(script, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Error: ${stderr}`);
-    } else {
-      console.log(`Output: ${stdout}`);
+const runFunctions = () => {
+  exec('node ./dev/tools/copyFiles.js');
+  exec('node ./dev/server/server.js');
+};
+
+const startServer = () => {
+  if (server) {
+    server.kill();
+  }
+
+  server = spawn('node', [join(__dirname, '../../dist/server/server.js')], {
+    stdio: 'inherit',
+  });
+
+  server.on('close', (code) => {
+    if (code !== null) {
+      console.log(`Server stopped with exit code ${code}`);
     }
   });
 };
 
-watch(join('./src'), { recursive: true }, (eventType, filename) => {
-  if (filename && ['.ts', '.html', '.css'].includes(extname(filename))) {
-    console.log(`${filename} file changed. Running script...`);
-    runScript();
-  }
-});
+const compileAndRun = () => {
+  exec('tsc', { cwd: join(__dirname, '../../') }, (err, stdout, stderr) => {
+    if (err) {
+    } else {
+      runFunctions();
+      startServer();
+    }
+  });
+};
 
-runScript();
+watch(
+  join(__dirname, '../../src'),
+  { recursive: true },
+  (eventType, filename) => {
+    if (filename && ['.ts', '.html', '.css'].includes(extname(filename))) {
+      console.log(
+        `${filename} file changed. Recompiling and restarting server...`
+      );
+      compileAndRun();
+    }
+  }
+);
+
+// Initial compile and run
+compileAndRun();
