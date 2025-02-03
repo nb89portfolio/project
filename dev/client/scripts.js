962aa9d3a8aa7e3fabe95804fb66cca96dcb7008
nb89portfolio <navraj.bains.89@outlook.com>
2025-02-02 21:53:03 -0500

    Trying to fix git.

--- dev/client/scripts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const id = 'app';
-function dom({ id }) {
-    const currentNode = document.getElementById(id);
-    const isNull = currentNode === null;
-    if (isNull) {
-        throw new Error();
-    }
-}
